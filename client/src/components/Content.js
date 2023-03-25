239ec2345c8dc9c9274d537ffccf2839d6fd2ced
Coby Lovelace <jtl5jjn@virginia.edu>
2023-03-25 17:34:57 -0400

    added upload image container

--- client/src/components/Content.js
+++ client/src/components/Content.js
@@ -4,6 +4,12 @@ function Content() {
   return (
     <div className="container">
         this is content
+        <div className="photo_upload">
+            <div>
+                <span>Upload image here</span>
+                <button>Upload</button>
+            </div>
+        </div>
     </div>
   );
 }
