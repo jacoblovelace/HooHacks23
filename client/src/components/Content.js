f2ac7856937737397a1dfdb29e53897ae26df028
Coby Lovelace <jtl5jjn@virginia.edu>
2023-03-25 16:34:08 -0400

    added navbar and content components

---
+++ client/src/components/Content.js
@@ -0,0 +1,11 @@
+import './Content.css';
+
+function Content() {
+  return (
+    <div className="container">
+        this is content
+    </div>
+  );
+}
+
+export default Content;
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <div className="nav">
+        Hello!
+    </div>
+  );
+}
+
+export default Navbar;
