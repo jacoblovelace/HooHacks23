c61ac8ad9fe1312b44b9536e3ff7bd0ffa35d002
Coby Lovelace <jtl5jjn@virginia.edu>
2023-03-26 02:20:49 -0400

    added additional item description info

--- client/src/components/ImageUpload.js
+++ client/src/components/ImageUpload.js
@@ -9,16 +9,16 @@ function ImageUpload() {
     const [item, setItem] = useState("");
 
     const itemDescriptions = {
-      "glass": "To recycle glass, rinse the container to remove any food or drink residue and remove any non-glass materials such as caps or lids. Then, take the clean glass to a recycling center or place it in a recycling bin designated for glass.",
-      "paper": "To dispose of paper properly, sort your paper waste into recyclable and non-recyclable categories. Recyclable paper products such as newspapers, magazines, and cardboard boxes should be placed in a recycling bin, while soiled or contaminated paper should be disposed of in the regular trash. For confidential documents, shred them before disposing of them. Always check with your local recycling guidelines to ensure proper disposal.",
-      "plastic": "To dispose of plastic properly, check the recycling number on the plastic item and rinse it with water to remove any food or drink residue. Different types of plastic have different recycling numbers and some may not be recyclable, so always check with your local recycling guidelines to ensure proper disposal. If the plastic item is recyclable, place it in a recycling bin designated for plastic. Non-recyclable plastic should be disposed of in the regular trash. Remember to reduce the use of single-use plastic items whenever possible to help minimize plastic waste."
+      "glass": [["recycle"], "To recycle glass, rinse the container to remove any food or drink residue and remove any non-glass materials such as caps or lids. Then, take the clean glass to a recycling center or place it in a recycling bin designated for glass."],
+      "paper": [["trash", "recycle"], "To dispose of paper properly, sort your paper waste into recyclable and non-recyclable categories. Recyclable paper products such as newspapers, magazines, and cardboard boxes should be placed in a recycling bin, while soiled or contaminated paper should be disposed of in the regular trash. For confidential documents, shred them before disposing of them. Always check with your local recycling guidelines to ensure proper disposal."],
+      "plastic": [["recycle", "trash"], "To dispose of plastic properly, check the recycling number on the plastic item and rinse it with water to remove any food or drink residue. Different types of plastic have different recycling numbers and some may not be recyclable, so always check with your local recycling guidelines to ensure proper disposal. If the plastic item is recyclable, place it in a recycling bin designated for plastic. Non-recyclable plastic should be disposed of in the regular trash. Remember to reduce the use of single-use plastic items whenever possible to help minimize plastic waste."]
     }
 
     const identifyItem = item_image => {
       // algorithm to identify image category using machine learning
 
       // once item is identified, setItem to identified item
-      setItem("glass");
+      setItem("plastic");
     }
   
     const handleDrop = acceptedFiles => {
@@ -43,7 +43,10 @@ function ImageUpload() {
       return (
         <div>
           <h3 style={{textTransform: "capitalize"}}>{item}</h3>
-          <p>{itemDescriptions[item]}</p>
+          <div className="disposal-types">
+            {itemDescriptions[item][0].map((type) => <h3 className={type}>{type}</h3>)}
+          </div>
+          <p>{itemDescriptions[item][1]}</p>
         </div>
       );
     }
