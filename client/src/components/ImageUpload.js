bb40283efbc7f918f305e7e12615228b32b5481d
Coby Lovelace <jtl5jjn@virginia.edu>
2023-03-25 21:18:59 -0400

    cleaned up the upload photo feature

--- client/src/components/ImageUpload.js
+++ client/src/components/ImageUpload.js
@@ -1,44 +1,64 @@
 import React, { useState } from 'react';
 import Dropzone from 'react-dropzone';
 import ImageGallery from 'react-image-gallery';
+import 'react-image-gallery/styles/css/image-gallery.css';
+import './ImageUpload.css';
 
 function ImageUpload() {
-  const [image, setImage] = useState(null);
-
-  const handleDrop = acceptedFiles => {
-    setImage(acceptedFiles[0]);
-  };
-
-  let images = [];
-  if (image) {
-    images = [
-      {
-        original: URL.createObjectURL(image),
-        thumbnail: URL.createObjectURL(image),
-        // any other details you want to display
-      }
-    ];
-  }
-
-  return (
-    <div>
-      <Dropzone onDrop={handleDrop}>
-        {({getRootProps, getInputProps}) => (
-            <div {...getRootProps()}>
+    const [image, setImage] = useState(null);
+  
+    const handleDrop = acceptedFiles => {
+      setImage(acceptedFiles[0]);
+    };
+  
+    const renderImage = (item) => {
+      return (
+        <div className="image-wrapper">
+          <img
+            src={item.original}
+            alt={item.originalAlt}
+            className="custom-image"
+          />
+        </div>
+      );
+    }
+  
+    let images = [];
+    if (image) {
+      images = [
+        {
+          original: URL.createObjectURL(image),
+          thumbnail: false,
+        }
+      ];
+    }
+  
+    return (
+        <div>
+        {image ? (
+          <ImageGallery
+            items={images}
+            showNav={false}
+            showPlayButton={false}
+            showFullscreenButton={false}
+            renderItem={renderImage}
+          />
+        ) : (
+          <Dropzone onDrop={handleDrop}>
+            {({getRootProps, getInputProps}) => (
+              <div {...getRootProps()}>
                 <div className="photo_upload">
-                    <div>
-                        <span>Upload image here</span>
-                        <button>Upload</button>
-                    </div>
+                  <div>
+                    <span>Click here or drag n' drop to upload image</span>
+                    <input {...getInputProps()} />
+                  </div>
                 </div>
-                <input {...getInputProps()} />
-                <p>Drag 'n' drop an image here, or click to select an image</p>
-            </div>
+              </div>
+            )}
+          </Dropzone>
         )}
-      </Dropzone>
-      <ImageGallery items={images} />
-    </div>
-  );
-}
+      </div>
+    );
+  }
 
 export default ImageUpload;
